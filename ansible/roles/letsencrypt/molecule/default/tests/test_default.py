39a2e743c73a7320b2233458edb4ebddf8e9cd50
Mads Jensen <mje@inducks.org>
2018-12-29 11:37:50 +0100

    Ansible role for LetsEncrypt

--- ansible/roles/letsencrypt/molecule/default/tests/test_default.py
+++ ansible/roles/letsencrypt/molecule/default/tests/test_default.py
@@ -6,9 +6,37 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_renewal_job_exists(host):
+    job = host.file('/etc/cron.daily/cert-renew.sh')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert job.exists
+    assert job.user == 'root'
+    assert job.group == 'root'
+    assert job.mode == '0755'
+
+
+def test_certbot_installed(host):
+    package = host.package('certbot')
+    assert package.is_installed
+
+
+def test_certbot_configuration(host):
+    configuration = host.file('/etc/letsencrypt/renewal/www.comicwiki.dk.conf')
+
+    assert configuration.exists
+    assert configuration.user == 'root'
+    assert configuration.group == 'root'
+    assert configuration.mode == '0644'
+
+    assert configuration.readlines().contains('')
+
+
+def test_certbot_configuration(host):
+    cli = host.file('/etc/letsencrypt/cli.ini')
+
+    assert cli.exists
+    assert cli.user == 'root'
+    assert cli.group == 'root'
+    assert cli.mode == '0644'
+
+    assert cli.readlines().contains('max-log-backups = 50')
